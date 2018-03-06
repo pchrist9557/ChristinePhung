1b464dd5014525e809b94c09b36f203ca95c81b1
Christine Phung <pchrist9557@gmail.com>
2018-03-06 15:12:36 -0600

    BusinessCard

---
+++ BusinessCard.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var name = "Christine Phung";
+var title = "Web Developer";
+var company = "Coolest Company";
+var phone = "555-555-5555";
+var email = "christine@coolestcompany.com";
+
+
+console.log(name);
+console.log("I am a " + title + " at the " + company);
+console.log("phone: " + phone);
+console.log("email: " + email);
